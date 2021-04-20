9b3202f7f1c60a420080c4d6a455482fd98beee5
Shoshana01 <shanabjacobs@gmail.com>
2021-04-19 20:36:41 -0400

    creating pictures in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Car.create(
+    owner: "Jonalyn",
+    model: "Lamborghini",
+    color: "BabyBlue"
+)
+Car.create(
+    owner: "Samuel",
+    model: "Tesla",
+    color: "Black"
+)
+Car.create(
+    owner: "Phillip",
+    model: "Ferrari",
+    color: "Red"
+)
