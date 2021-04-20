53c5356b04713b6cc1e96fdaf9a412867b3cf211
Shoshana01 <shanabjacobs@gmail.com>
2021-04-19 21:58:32 -0400

    Adding feature to create new picture using HTML form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,11 @@ class CarsController < ApplicationController
     def show
         @car = Car.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        render plain: "Saving a car. Owner: #{params[:owner]}, Model: #{params[:model]}, Color: #{params[:color]}"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'cars' => 'cars#index' 
+
+  post 'cars' => 'cars#create'
+  get 'cars/new' => 'cars#new'
+  
   get 'cars/:id' => 'cars#show', as: 'car'
+
 end
