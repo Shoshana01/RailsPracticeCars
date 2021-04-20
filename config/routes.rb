a7cb970d4e48d138e076ab5814c8737b42de5a44
Shoshana01 <shanabjacobs@gmail.com>
2021-04-19 22:18:32 -0400

    Configuring root as cars#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'cars#index'
+
   get 'cars' => 'cars#index' 
 
   post 'cars' => 'cars#create'
   get 'cars/new' => 'cars#new'
-  
+
   get 'cars/:id' => 'cars#show', as: 'car'
 
 end
